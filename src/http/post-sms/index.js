da38c3c754214a6f435f2728cf4d51fed5a91b98
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-01-15 20:22:48 -0800

    Use game

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,7 +1,29 @@
+let arc = require('@architect/functions');
+let twilio = require('@architect/shared/twilio');
+let game = require('./gameplay.js');
+
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    status: 302,
-    location: '/'
+  if ((process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'staging') && !twilio.validateRequest(req)) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized'
+    }
+  }
+
+  console.log(Date());
+  try {
+    response = await game(arc.http.helpers.bodyParser(req));
+    return {
+      statusCode: 200,
+      headers: response.headers,
+      body: 'body' in response ? response.body.toString() : ''
+    }
+  }
+  catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: 'Internal server error'
+    }
   }
 }
