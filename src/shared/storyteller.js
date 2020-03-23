f5b9af1b0c3a3f48d3699e27269e4b83526bde6c
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-03-22 17:55:34 -0700

    Forgot Storyteller

---
+++ src/shared/storyteller.js
@@ -0,0 +1,19 @@
+const rp = require('request-promise-native');
+
+async function nextWord(word) {
+    // Use Datamase Words API with left context mode. @see https://www.datamuse.com/api/
+    let result = await rp({
+        url: 'https://api.datamuse.com/words?lc=' + word,
+        method: 'GET',
+        json: true
+    })
+    .catch(function (err) {
+        throw new Error('Datamuse request error:', err);
+    });
+    result = result[Math.floor(Math.random() * Math.floor(result.length))];
+    return result.word;
+}
+
+module.exports = {
+    nextWord: nextWord
+}
