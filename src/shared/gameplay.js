6a38f18e7a152c033370d4f87807e91432421d40
Ben Jeavons <bjeavons@users.noreply.github.com>
2020-03-25 15:46:26 -0700

    UX improvements during pregame, regular players can stop instead of end

--- src/shared/gameplay.js
+++ src/shared/gameplay.js
@@ -57,7 +57,7 @@ async function intro(sender, channel, command, message) {
             // No game yet so set it up on token.
             let player_id = await gameplayer.create(sender, channel, 'pending_game'); // @todo make const
             await storytime.setup(player_id, message.toLowerCase());
-            response = "b: Welcome to Storytime! Once at least one more person joins you'll be collaboratively telling a short story over SMS. Learn more about what's going to happen, text back /intro (with slash).";
+            response = "b: Welcome to Storytime! Once at least one more person joins you can start telling a short story together over texts to this number. Learn more about what's going to happen, text back /intro (with slash).";
         }
         else if (!storytime.hasStarted(game)) {
             let player_id = await gameplayer.create(sender, channel, 'pending_game'); // @todo make const
@@ -124,10 +124,13 @@ async function pregame(player, command, message) {
     const game = await storytime.get(player.key);
 
     if (command.help && !storytime.isCreator(game, player.key)) {
-        response = "b: You're currently a player in a game waiting to start. Text /end to leave it now (and at any time) or hang tight for the game creator to get it started! FYI msg&data rates may apply.";
+        response = "b: You're currently a player in a game waiting to start. Text /stop to leave it now (or at any time) otherwise hang tight for the game creator to get it started! Text /intro to learn what's going to happen. FYI msg&data rates may apply.";
     }
     else if (command.intro && !storytime.isCreator(game, player.key)) {
-        response = "b: This is Storytime, a collaborative storytelling game over SMS. You and your friends tell a short story, one or two words at a time, by text messages to this number. When someone texts the next part of the story the whole story gets sent to another player to add on to! Text /end to leave the game at any time. Text /help for help.";
+        response = "b: This is Storytime, a collaborative storytelling game over SMS. You and your friends tell a short story, one or two words at a time, by text messages to this number. When someone texts the next part of the story the whole story gets sent to another player to add on to! Text /stop to leave the game at any time. Text /help for help.";
+    }
+    else if (command.intro && storytime.isCreator(game, player.key)) {
+        response = "When you start the story I'll ask you to reply with one or two words that I'll send to another player. What they respond with is added to your words and sent together to the next player, and so the story continues! For now, short stories are best :) End the game at any time by texting just /end.";
     }
     else if (command.start && storytime.isCreator(game, player.key)) {
         console.log("Starting game!");
@@ -143,17 +146,17 @@ async function pregame(player, command, message) {
         response = "b: Ending before it begins? OK, no problem. Text back the game token to restart.";
         await storytime.end(player.key);
     }
-    else if (!storytime.hasStarted(game) && storytime.isCreator(game, player.key)) {
+    else if (storytime.isCreator(game, player.key)) {
         let playerCount = storytime.getPlayerCount(game);
         if (playerCount === 1) {
-            response = "b: There are no other players yet. Get some people to join!";
+            response = "b: There are no other players yet. Get some people to join by asking them to text the token to this number! When at least one more person joins you can start the game by texting /start.";
         }
         else {
-            response = "b: The game is ready with " + playerCount + " players, get some more people to join or start the game by texting /start!";
+            response = "b: The game is ready with " + playerCount + " players, get some more people to join by asking them to text the token to this number! When you're ready you can start the game by texting /start.";
         }
-        response += " FYI msg&data rates may apply.";
+        response += " Text /end to end the game at any time. FYI msg&data rates may apply.";
     }
-    else if (!storytime.hasStarted(game)) {
+    else {
         console.log("Message ignored, game hasn't started.");
         response = "b: Hi there, thanks for your message, I'm waiting for the game creator to let me get the storytelling game started. Hang tight! Text /help for help.";
     }
